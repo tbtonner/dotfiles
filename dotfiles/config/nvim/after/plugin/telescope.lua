ae25540fc5bbd2f2e892399ea5601003d73af037
Tom Tonner <tom.tonner@couchbase.com>
2024-08-07 12:35:25 +0100

    References add to method too

--- dotfiles/config/nvim/after/plugin/telescope.lua
+++ dotfiles/config/nvim/after/plugin/telescope.lua
@@ -8,7 +8,7 @@ require('telescope').setup {
     }
 }
 
-local implementations = {
+local ignoremocks = {
     on_complete = {
         function(picker)
             picker:clear_completion_callbacks()
@@ -18,7 +18,8 @@ local implementations = {
         end,
     },
     initial_mode = "normal",
-    show_line = false
+    show_line = false,
+    include_current_line = false
 }
 
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
@@ -27,8 +28,8 @@ vim.keymap.set("n", "<leader>tr", ":Telescope resume<cr>")
 
 vim.keymap.set("n", "gb", function() builtin.buffers({ initial_mode = "normal", show_line = false }) end, {})
 vim.keymap.set("n", "gd", function() builtin.lsp_definitions({ initial_mode = "normal", show_line = false }) end, {})
-vim.keymap.set("n", "gi", function() builtin.lsp_implementations(implementations) end, {})
-vim.keymap.set("n", "gr", function() builtin.lsp_references({ initial_mode = "normal", show_line = false, include_current_line = false }) end, {})
+vim.keymap.set("n", "gi", function() builtin.lsp_implementations(ignoremocks) end, {})
+vim.keymap.set("n", "gr", function() builtin.lsp_references(ignoremocks) end, {})
 vim.keymap.set("n", "ge", function() builtin.diagnostics({ initial_mode = "normal", show_line = false }) end, {})
 
 vim.keymap.set("n", "gf", function() builtin.treesitter({ initial_mode = "normal" }) end, {})
