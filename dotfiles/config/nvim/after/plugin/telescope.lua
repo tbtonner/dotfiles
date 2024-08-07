7e265acc320983ea34cc60702eee9b66af298e4f
Tom Tonner <tom.tonner@couchbase.com>
2024-08-07 12:01:35 +0100

    Change current line and implementaions to correct place

--- dotfiles/config/nvim/after/plugin/telescope.lua
+++ dotfiles/config/nvim/after/plugin/telescope.lua
@@ -27,8 +27,8 @@ vim.keymap.set("n", "<leader>tr", ":Telescope resume<cr>")
 
 vim.keymap.set("n", "gb", function() builtin.buffers({ initial_mode = "normal", show_line = false }) end, {})
 vim.keymap.set("n", "gd", function() builtin.lsp_definitions({ initial_mode = "normal", show_line = false }) end, {})
-vim.keymap.set("n", "gi", function() builtin.lsp_implementations({ initial_mode = "normal", show_line = false }) end, {})
-vim.keymap.set("n", "gr", function() builtin.lsp_references(implementations) end, {})
+vim.keymap.set("n", "gi", function() builtin.lsp_implementations(implementations) end, {})
+vim.keymap.set("n", "gr", function() builtin.lsp_references({ initial_mode = "normal", show_line = false, include_current_line = false }) end, {})
 vim.keymap.set("n", "ge", function() builtin.diagnostics({ initial_mode = "normal", show_line = false }) end, {})
 
 vim.keymap.set("n", "gf", function() builtin.treesitter({ initial_mode = "normal" }) end, {})
