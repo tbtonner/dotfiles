131c40601866e2e4bd49cdf9c7a4db7d7bd9a94c
Tom Tonner <tom.tonner@couchbase.com>
2024-08-08 16:27:11 +0100

    Remove pointless func

--- dotfiles/config/nvim/after/plugin/telescope.lua
+++ dotfiles/config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,19 @@
 local builtin = require('telescope.builtin')
 
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set("n", "<leader>tr", ":Telescope resume<cr>")
+
+vim.keymap.set("n", "gb", function() builtin.buffers({ initial_mode = "normal", show_line = false }) end, {})
+vim.keymap.set("n", "gd", function() builtin.lsp_definitions({ initial_mode = "normal", show_line = false }) end, {})
+vim.keymap.set("n", "gi", function() builtin.lsp_implementations({ initial_mode = "normal", show_line = false }) end, {})
+vim.keymap.set("n", "gr", function() builtin.lsp_references({ initial_mode = "normal", show_line = false }) end, {})
+vim.keymap.set("n", "ge", function() builtin.diagnostics({ initial_mode = "normal", show_line = false }) end, {})
+
+vim.keymap.set("n", "gf", function() builtin.treesitter({ initial_mode = "normal" }) end, {})
+vim.keymap.set("n", "gj", function() builtin.jumplist({ initial_mode = "normal", show_line = false }) end, {})
+vim.keymap.set("n", "gm", function() builtin.marks({ initial_mode = "normal" }) end, {})
+
 require('telescope').setup {
     defaults = {
         file_ignore_patterns = {
@@ -7,33 +21,5 @@ require('telescope').setup {
         }
     }
 }
-
-local implementationsAndRefs = {
-    on_complete = {
-        function(picker)
-            picker:clear_completion_callbacks()
-            if picker.manager.linked_states.size == 1 then
-                require("telescope.actions").select_default(picker.prompt_bufnr)
-            end
-        end,
-    },
-    initial_mode = "normal",
-    show_line = false
-}
-
-
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
-vim.keymap.set("n", "<leader>tr", ":Telescope resume<cr>")
-
-vim.keymap.set("n", "gb", function() builtin.buffers({ initial_mode = "normal", show_line = false }) end, {})
-vim.keymap.set("n", "gd", function() builtin.lsp_definitions({ initial_mode = "normal", show_line = false }) end, {})
-vim.keymap.set("n", "gi", function() builtin.lsp_implementations(implementationsAndRefs) end, {})
-vim.keymap.set("n", "gr", function() builtin.lsp_references(implementationsAndRefs) end, {})
-vim.keymap.set("n", "ge", function() builtin.diagnostics({ initial_mode = "normal", show_line = false }) end, {})
-
-vim.keymap.set("n", "gf", function() builtin.treesitter({ initial_mode = "normal" }) end, {})
-vim.keymap.set("n", "gj", function() builtin.jumplist({ initial_mode = "normal", show_line = false }) end, {})
-vim.keymap.set("n", "gm", function() builtin.marks({ initial_mode = "normal" }) end, {})
 
 require('telescope').load_extension('fzf')
