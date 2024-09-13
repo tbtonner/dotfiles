2e4fedf4f6f688562301227cbd80ab5f65500d4d
Tom Tonner <tom.tonner@couchbase.com>
2024-09-13 09:47:28 +0100

    Markdown preview

--- dotfiles/config/nvim/lua/vimsettings/lazy.lua
+++ dotfiles/config/nvim/lua/vimsettings/lazy.lua
@@ -43,6 +43,16 @@ require("lazy").setup({
     "tpope/vim-unimpaired",
 
     {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+
+    {
         "vim-test/vim-test",
         dependencies = {
             "preservim/vimux",
