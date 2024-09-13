442a7c6c8bc601547ab2c215c985a3cfff26562d
Tom Tonner <tom.tonner@couchbase.com>
2024-09-13 14:41:38 +0100

    Gitlink and markdown update

--- dotfiles/config/nvim/after/plugin/gitlink.lua
+++ dotfiles/config/nvim/after/plugin/gitlink.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>gl", ":GetCommitLink<cr>")
+vim.keymap.set("n", "<leader>gl", ":GetCommitLink<cr><cr>")
 vim.keymap.set("x", "<leader>gl", ":GetCommitLink<cr><cr>")
-vim.keymap.set("n", "<leader>gL", ":GetCurrentBranchLink<cr>")
+vim.keymap.set("n", "<leader>gL", ":GetCurrentBranchLink<cr><cr>")
 vim.keymap.set("x", "<leader>gL", ":GetCurrentBranchLink<cr><cr>")
--- dotfiles/config/nvim/after/plugin/markdown.lua
+++ dotfiles/config/nvim/after/plugin/markdown.lua
@@ -1,1 +1,3 @@
+vim.g.mkdp_auto_close = 0
+
 vim.keymap.set("n", "<leader>m", ":MarkdownPreviewToggle<cr>")
