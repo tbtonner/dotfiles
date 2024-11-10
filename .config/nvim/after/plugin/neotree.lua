8890385f400450123d1eb3e6fb93726b615f1680
Tom Tonner <tom.tonner@couchbase.com>
2024-11-10 13:14:32 +0000

    Update and hide gitignore

--- .config/nvim/after/plugin/neotree.lua
+++ .config/nvim/after/plugin/neotree.lua
@@ -9,9 +9,7 @@ require("neo-tree").setup({
     },
     filesystem = {
         filtered_items = {
-            visible = true,
             hide_dotfiles = false,
-            hide_gitignored = true,
         },
     }
 })
