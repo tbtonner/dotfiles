13f07fdfaaaef6a0fdcb2dfcbad8f2e28a419c16
Tom Tonner <tom.tonner@couchbase.com>
2025-07-28 14:59:15 +0100

    bufdo

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -84,8 +84,8 @@ vim.api.nvim_set_keymap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', { nore
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = { "*.go", "*.lua", "*.ts" },
     callback = function()
+        vim.lsp.buf.format()
         vim.lsp.buf.code_action { context = { only = { 'source.organizeImports' } }, apply = true }
         vim.lsp.buf.code_action { context = { only = { 'source.fixAll' } }, apply = true }
-        vim.lsp.buf.format()
     end
 })
