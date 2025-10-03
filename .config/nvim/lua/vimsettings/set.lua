279deb7915fd22cfca1adfec69249207ea905229
Tom Tonner <tom.tonner@couchbase.com>
2025-10-03 18:52:16 +0900

    update

--- .config/nvim/lua/vimsettings/set.lua
+++ .config/nvim/lua/vimsettings/set.lua
@@ -11,43 +11,52 @@ vim.opt.wrap           = true
 vim.opt.linebreak      = true
 vim.opt.wrapmargin     = 0
 vim.opt.showbreak      = "  "
+vim.opt.formatoptions:append("t")
 
-vim.opt.termguicolors  = true
+vim.opt.termguicolors = true
 
-vim.opt.autoindent     = true
-vim.opt.smartindent    = true
-vim.opt.breakindent    = true
+vim.opt.autoindent    = true
+vim.opt.smartindent   = true
+vim.opt.breakindent   = true
 
-vim.opt.cursorline     = true
+vim.opt.cursorline    = true
 
-vim.opt.ignorecase     = true
+vim.opt.ignorecase    = true
 
-vim.opt.tabstop        = 4
-vim.opt.softtabstop    = 4
-vim.opt.shiftwidth     = 4
-vim.opt.expandtab      = true
+vim.opt.tabstop       = 4
+vim.opt.softtabstop   = 4
+vim.opt.shiftwidth    = 4
+vim.opt.expandtab     = true
 
-vim.opt.encoding       = "UTF-8"
-vim.opt.spelllang      = "en"
-vim.opt.regexpengine   = 1
+vim.opt.encoding      = "UTF-8"
+vim.opt.spelllang     = "en"
+vim.opt.regexpengine  = 1
 
-vim.opt.clipboard      = 'unnamedplus'
+vim.opt.clipboard     = 'unnamedplus'
 
-vim.opt.swapfile       = false
-vim.opt.backup         = false
-vim.opt.undodir        = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile       = true
+vim.opt.swapfile      = false
+vim.opt.backup        = false
+vim.opt.undodir       = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile      = true
 
-vim.opt.hlsearch       = true
-vim.opt.incsearch      = true
+vim.opt.hlsearch      = true
+vim.opt.incsearch     = true
 
-vim.opt.scrolloff      = 10
-vim.opt.signcolumn     = "yes"
+vim.opt.scrolloff     = 10
+vim.opt.signcolumn    = "yes"
 
-vim.opt.updatetime     = 50
+vim.opt.updatetime    = 50
 
-vim.g.history          = 1000
+vim.g.history         = 1000
 
-vim.g.mapleader        = " "
+vim.g.mapleader       = " "
 
-vim.opt.shell          = "/bin/bash"
+vim.opt.shell         = "/bin/bash"
+
+-- Automatically set 'textwidth' formatting option for all buffers
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*",
+    callback = function()
+        vim.opt.formatoptions:append("t")
+    end,
+})
