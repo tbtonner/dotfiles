e9c9dc3be23f0b2ffd567fe7927d7abedceed358
Tom Tonner <tom.tonner@couchbase.com>
2025-10-22 14:45:58 +0900

    Update neotree back to small

--- .config/nvim/after/plugin/neotree.lua
+++ .config/nvim/after/plugin/neotree.lua
@@ -1,6 +1,12 @@
 require("neo-tree").setup({
     event_handlers = {
         {
+            event = "file_open_requested",
+            handler = function()
+                require("neo-tree.command").execute({ action = "close" })
+            end
+        },
+        {
             event = "neo_tree_buffer_enter",
             handler = function()
                 vim.opt_local.relativenumber = true
@@ -36,4 +42,4 @@ require("neo-tree").setup({
     },
 })
 
-vim.keymap.set("n", "<F1>", ":Neotree filesystem reveal current toggle<cr>")
+vim.keymap.set("n", "<F1>", ":Neotree filesystem reveal toggle<cr>")
