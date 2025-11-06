4505e41bfe35da156693f34637c58b2d4c047857
Tom Tonner <tom.tonner@couchbase.com>
2025-11-06 10:56:18 +0900

    Remove copilot

--- .config/nvim/after/plugin/gitlink.lua
+++ .config/nvim/after/plugin/gitlink.lua
@@ -1,4 +1,1 @@
-vim.keymap.set("n", "<leader>gl", ":GetCommitLink<cr><cr>")
-vim.keymap.set("x", "<leader>gl", ":GetCommitLink<cr><cr>")
-vim.keymap.set("n", "<leader>gL", ":GetCurrentBranchLink<cr><cr>")
-vim.keymap.set("x", "<leader>gL", ":GetCurrentBranchLink<cr><cr>")
+require "gitlinker".setup({})
