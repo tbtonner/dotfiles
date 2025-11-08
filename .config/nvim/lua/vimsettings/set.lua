c0339dfc8156fd7bc2541352d4c9f01808816234
Tom Tonner <tom.tonner@couchbase.com>
2025-11-08 21:15:34 +0900

    vim settings

--- .config/nvim/lua/vimsettings/set.lua
+++ .config/nvim/lua/vimsettings/set.lua
@@ -1,55 +1,31 @@
-vim.opt.nu             = true
-vim.opt.relativenumber = true
+vim.g.mapleader        = " "
 
-vim.opt.mousescroll    = "ver:1,hor:0"
+vim.opt.encoding       = "UTF-8"
+vim.opt.spelllang      = "en"
+
+vim.opt.clipboard      = 'unnamedplus'
+
+vim.opt.relativenumber = true
+vim.opt.termguicolors  = true
+vim.opt.cursorline     = true
+vim.opt.showmode       = false
+
+vim.opt.ignorecase     = true
 
 vim.opt.ruler          = true
 vim.opt.colorcolumn    = "120"
-vim.opt.textwidth      = 120
 
-vim.opt.wrap           = true
-vim.opt.linebreak      = true
-vim.opt.wrapmargin     = 0
-vim.opt.showbreak      = "  "
-vim.opt.formatoptions:append("t")
+vim.opt.autoindent     = true
+vim.opt.smartindent    = true
+vim.opt.breakindent    = true
 
-vim.opt.termguicolors = true
+vim.opt.tabstop        = 4
+vim.opt.softtabstop    = 4
+vim.opt.shiftwidth     = 4
+vim.opt.expandtab      = true
 
-vim.opt.autoindent    = true
-vim.opt.smartindent   = true
-vim.opt.breakindent   = true
+vim.opt.hlsearch       = true
+vim.opt.incsearch      = true
 
-vim.opt.cursorline    = true
-
-vim.opt.showmode      = false
-
-vim.opt.ignorecase    = true
-
-vim.opt.tabstop       = 4
-vim.opt.softtabstop   = 4
-vim.opt.shiftwidth    = 4
-vim.opt.expandtab     = true
-
-vim.opt.encoding      = "UTF-8"
-vim.opt.spelllang     = "en"
-
-vim.opt.clipboard     = 'unnamedplus'
-
-vim.opt.swapfile      = false
-vim.opt.backup        = false
-vim.opt.undodir       = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile      = true
-
-vim.opt.hlsearch      = true
-vim.opt.incsearch     = true
-
-vim.opt.scrolloff     = 10
-vim.opt.signcolumn    = "yes"
-
-vim.opt.updatetime    = 50
-
-vim.g.history         = 1000
-
-vim.g.mapleader       = " "
-
-vim.opt.shell         = "/bin/bash"
+vim.opt.scrolloff      = 10
+vim.opt.updatetime     = 50
