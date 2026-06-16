e563c918ebf4743281b6562556cbc49605d2d5d2
Tom Tonner <tom.tonner@couchbase.com>
2026-06-16 13:35:17 +0900

    colours gitsigns blame

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,11 +1,39 @@
 local noice = require("noice")
 
+-- Kanagawa wave palette
+local blame_colors = {
+    "#7E9CD8",
+    "#FFA066",
+    "#98BB6C",
+    "#E6C384",
+    "#957FB8",
+    "#E46876",
+    "#7AA89F",
+    "#7FB4CA",
+    "#D27E99",
+    "#9CABCA",
+}
+
+local function hash_to_color(hash)
+    if not hash or hash == "" or hash:match("^0+$") then
+        return "#727169"
+    end
+    local n = 0
+    for i = 1, #hash do n = n + string.byte(hash, i) end
+    return blame_colors[(n % #blame_colors) + 1]
+end
+
 local function gitsigns_blame()
     local line = vim.b.gitsigns_blame_line
     if line ~= nil then
+        local dict = vim.b.gitsigns_blame_line_dict
+        local sha = dict and dict.sha
         vim.b._blame_cache = line
-        return line
+        if sha and sha ~= "" then
+            vim.b._blame_hash_cache = sha
+        end
     end
+    vim.b._blame_hash = vim.b._blame_hash_cache or ""
     return vim.b._blame_cache or ""
 end
 
@@ -29,14 +57,19 @@ require('lualine').setup {
             'diff',
             'diagnostics',
             ---@diagnostic disable-next-line: undefined-field
-            { noice.api.status.mode.get, cond = noice.api.status.mode.has, color = { fg = "#ff9e64" }, },
+            { noice.api.status.mode.get, cond = noice.api.status.mode.has, color = { fg = "#FFA066" }, },
         },
         lualine_c = { { 'filename', path = 1 } },
 
         lualine_x = {
-            gitsigns_blame,
-            { quickfix_position, color = { fg = "#e0af68" } },
-            { searchcount, color = { fg = "#7dcfff" } },
+            {
+                gitsigns_blame,
+                color = function()
+                    return { fg = hash_to_color(vim.b._blame_hash) }
+                end,
+            },
+            { quickfix_position, color = { fg = "#E6C384" } },
+            { searchcount, color = { fg = "#7E9CD8" } },
             'selectioncount',
             'filetype',
         },
